bc5a6afbaa7864fbed152c0aa9f8669b29ad07ab
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-02-25 11:58:41 +0200

    added LCD and liquid crystal library

--- arduino/src/main/aduino/aduino.ino
+++ arduino/src/main/aduino/aduino.ino
@@ -1,16 +1,24 @@
 #include <DHT.h>
 #include "MQ135.h"
+#include <LiquidCrystal.h>
 
 #define DHT_PIN 2
 
 MQ135 gasSensor = MQ135(A1);
 DHT dht(DHT_PIN, DHT22);
+LiquidCrystal lcd(7, 8, 10, 11, 12, 13);
+// LiquidCrystal(rs, enable, d4, d5, d6, d7)
 
 int KY037_DIGITAL_PIN = 6;
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16,2);
   dht.begin();
+  lcd.clear();
+  lcd.print("Hello, A");
+  lcd.setCursor(0, 1);
+  lcd.print("rduino!");
 }
 
 
@@ -19,7 +27,8 @@ void loop() {
   float humidity = dht.readHumidity();
   float ppm = gasSensor.getCorrectedPPM(temperature, humidity);
   
-  int dNoiseLevel = digitalRead(KY037_DIGITAL_PIN); // 0 || 1
+  int dNoiseLevel = digitalRead(KY037_DIGITAL_PIN); 
+  // 0 || 1
 
   Serial.println("Temperature: " + String(temperature) + "°C, Humidity: " + String(humidity) + "%, CO PPM: " + String(ppm));
 
