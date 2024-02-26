53f94f9b6a570a58a1df1f2fd4b4540def728b84
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-02-26 14:56:51 +0200

    added led diod and printig sensor info on lcd

--- arduino/aduino/aduino.ino
+++ arduino/aduino/aduino.ino
@@ -4,7 +4,6 @@
 
 #define DHT_PIN 2
 
-
 MQ135 gasSensor = MQ135(A1);
 DHT dht(DHT_PIN, DHT22);
 LiquidCrystal lcd(7, 8, 10, 11, 12, 13);
@@ -12,47 +11,79 @@ LiquidCrystal lcd(7, 8, 10, 11, 12, 13);
 
 int KY037_DIGITAL_PIN = 6;
 int KY037_ANALOG_PIN = A0;
+int LED_PIN_1 = 3;
+int resistor = 220;
 
 void setup() {
   dht.begin();
 
   lcd.begin(16, 2);
-  lcd.clear();
-  lcd.print("Hello, A");
-  lcd.setCursor(0, 1);
-  lcd.print("rduino!");
 
   pinMode(KY037_DIGITAL_PIN, INPUT);
   pinMode(KY037_ANALOG_PIN, INPUT);
-  
+
+  pinMode(LED_PIN_1, OUTPUT);
+
   Serial.begin(9600);
 }
 
-char* KY037AnalogTodBParser(int value) {
-    if (value <= 327) {
-        return "< 32.00 dB";
-    } else if (value >= 328 && value <= 351) {
-        return "32.01 - 35.00 dB";
-    } else if (value >= 352 && value <= 440) {
-        return "35.01 - 44.00 dB";
-    } else if (value >= 441 && value <= 483) {
-        return "44.01 - 49.00 dB";
-    } else if (value >= 484 && value <= 544) {
-        return "49.01 - 53.00 dB";
-    } else if (value >= 545 && value <= 550) {
-        return "53.01 - 56.00 dB";
-    } else if (value >= 551 && value <= 611) {
-        return "56.01 - 61.00 dB";
-    } else if (value >= 612 && value <= 662) {
-        return "61.01 - 65.00 dB";
-    } else if (value >= 663 && value <= 764) {
-        return "65.01 - 71.00 dB";
-    } else if (value >= 765 && value <= 770) {
-        return "71.01 - 78.00 dB";
-    } else {
-        return "> 72.00 dB";
-    }
+char* KY037AnalogTodBParserWeb(int value) {
+  if (value <= 327) {
+    return "< 32.00 dB";
+  } else if (value >= 328 && value <= 351) {
+    return "32.01 - 35.00 dB";
+  } else if (value >= 352 && value <= 440) {
+    return "35.01 - 44.00 dB";
+  } else if (value >= 441 && value <= 483) {
+    return "44.01 - 49.00 dB";
+  } else if (value >= 484 && value <= 544) {
+    return "49.01 - 53.00 dB";
+  } else if (value >= 545 && value <= 550) {
+    return "53.01 - 56.00 dB";
+  } else if (value >= 551 && value <= 611) {
+    return "56.01 - 61.00 dB";
+  } else if (value >= 612 && value <= 662) {
+    return "61.01 - 65.00 dB";
+  } else if (value >= 663 && value <= 764) {
+    return "65.01 - 71.00 dB";
+  } else if (value >= 765 && value <= 770) {
+    return "71.01 - 78.00 dB";
+  } else {
+    return "> 72.00 dB";
+  }
 }
+
+char* KY037AnalogTodBParserDisplay(int value) {
+  if (value <= 327) {
+    return "<32";
+  } else if (value >= 328 && value <= 351) {
+    return "32.1-35";
+  } else if (value >= 352 && value <= 440) {
+    return "35.1-44";
+  } else if (value >= 441 && value <= 483) {
+    return "44.1-49";
+  } else if (value >= 484 && value <= 544) {
+    return "49.1-53";
+  } else if (value >= 545 && value <= 550) {
+    return "53.1-56";
+  } else if (value >= 551 && value <= 611) {
+    return "56.1-61";
+  } else if (value >= 612 && value <= 662) {
+    return "61.1-65";
+  } else if (value >= 663 && value <= 764) {
+    return "65.1-71";
+  } else if (value >= 765 && value <= 770) {
+    return "71.1-78";
+  } else {
+    return ">72";
+  }
+}
+
+int calculateBrightness(int noiseLevel, int maxResistorValue) {
+  return map(noiseLevel, 0, 1, 0, maxResistorValue);
+}
+
+int counter = 0;
 
 void loop() {
   float temperature = dht.readTemperature();
@@ -60,11 +91,46 @@ void loop() {
   float ppm = gasSensor.getCorrectedPPM(temperature, humidity);
 
   int dNoiseLevel = digitalRead(KY037_DIGITAL_PIN);  // 0 || 1
-  int aNoiseLevel = analogRead(KY037_ANALOG_PIN); // 0 - 1023
-  char* dBNoiseLevel = KY037AnalogTodBParser(aNoiseLevel);
+  int aNoiseLevel = analogRead(KY037_ANALOG_PIN);    // 0 - 1023
+  char* dBNoiseLevelWeb = KY037AnalogTodBParserWeb(aNoiseLevel);
+  char* dBNoiseLevelDisplay = KY037AnalogTodBParserDisplay(aNoiseLevel);
 
-  Serial.println(String(temperature) + " °C, " + String(humidity) + " %, " + String(ppm) + " ppm, " + String(dBNoiseLevel) + ", " + String(aNoiseLevel));
-  // Serial.println(aNoiseLevel);
+  int ledBrightness = calculateBrightness(dNoiseLevel, resistor);
+  analogWrite(LED_PIN_1, ledBrightness);
+
+  if (dNoiseLevel == 1) {
+    digitalWrite(LED_PIN_1, HIGH);
+  } else {
+    digitalWrite(LED_PIN_1, LOW);
+  }
+
+  lcd.clear();
+  if (counter < 200) {
+    lcd.print(temperature);
+    lcd.setCursor(0, 1);
+    lcd.print("C");
+  } else if (counter < 400) {
+    lcd.print(humidity);
+    lcd.setCursor(0, 1);
+    lcd.print("%");
+  } else if (counter < 800) {
+    lcd.print(ppm);
+    lcd.setCursor(0, 1);
+    lcd.print("ppm");
+  } else if (counter < 1200) {
+    lcd.print(dBNoiseLevelDisplay);
+    lcd.setCursor(0, 1);
+    lcd.print("dB");
+  } else if (counter == 1201) {
+    counter = 0;
+    lcd.print(temperature);
+    lcd.setCursor(0, 1);
+    lcd.print("C");
+  }
+
+  Serial.println(String(temperature) + " °C, " + String(humidity) + " %, " + String(ppm) + " ppm, " + String(dBNoiseLevelWeb) + ", " + String(aNoiseLevel));
+
+  counter++;
 
   delay(25);
 }
