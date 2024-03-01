3e2c5dd9b830015d66ae79ed55b05c71679e9ed9
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-01 17:43:45 +0200

    official documentation examples for screenshots

---
+++ examples/arduinoWorkflow.ts
@@ -0,0 +1,15 @@
+const arduinoWorkflow: () => void = () => {
+    const setup = () => {
+        // put your setup code here, to run once:
+    }
+    const loop: () => any = () => {
+        // put your main code here, to run repeatedly:
+    }
+    setup();
+    while(true) {
+        loop();
+    }
+}
+
+
+
---
+++ examples/exmp.ino
@@ -0,0 +1,15 @@
+void main() {
+    setup();
+    while (1)
+    {
+        loop();
+    }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
